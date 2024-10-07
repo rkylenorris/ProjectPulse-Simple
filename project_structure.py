a5bdefb150875b8824752283c5f3729cf402789a
Kyle Norris <r.kyle.norris@gmail.com>
2024-10-07 08:52:05 -0400

    added file to classes project_structure.py

--- project_structure.py
+++ project_structure.py
@@ -1,1 +1,7 @@
 # TODO create classes for Project, Task, and Milestone
+
+class Project:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
